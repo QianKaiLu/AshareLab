2d7329482e24a5976309c84918cc9eb663b02111
qiankailu <ntqkhh@163.com>
2025-10-31 20:11:20 +0800

    stock news em

--- datas/see_summary.py
+++ datas/see_summary.py
@@ -1,8 +1,11 @@
 import akshare as ak
 
 def main():
-    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="002594", period="daily", start_date="20251001", end_date='20251027', adjust="")
-    print(stock_zh_a_hist_df.dtypes)
+    # stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="002594", period="daily", start_date="20251001", end_date='20251027', adjust="")
+    # print(stock_zh_a_hist_df.dtypes)
+    
+    stock_news_df= ak.stock_news_em(symbol="002460")
+    stock_news_dict = stock_news_df.to_dict(orient='records')
 
 if __name__ == "__main__":
     main()

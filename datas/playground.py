e8f44dd809f24d061a26661866f68ab0c02d15e5
qiankailu <ntqkhh@163.com>
2025-11-17 15:11:04 +0800

    workflow analyses stock

--- datas/playground.py
+++ datas/playground.py
@@ -4,12 +4,36 @@ import pandas as pd
 import akshare as ak
 from datas.query_stock import query_latest_bars
 from pathlib import Path
+import yfinance as yf
+import requests
+from newsapi import NewsApiClient
 
-# 沪深300成分股
-df_300 = ak.index_stock_cons(symbol="000300")
-print("沪深300成分股：")
-print(df_300.count())
-# 中证500成分股
-df_500 = ak.index_stock_cons(symbol="000905")
-print("中证500成分股：")
-print(df_500.count())
+# newsapi = NewsApiClient(api_key='dc5b76fac93f4740ab3ec029c4ceeb44')
+# results = newsapi.get_everything(q='小米', language='zh')
+# print(results)
+
+# # 获取顶部头条新闻
+# top_headlines = newsapi.get_top_headlines(
+#     q='technology',            # 关键词（可选）
+#     sources='bbc-news,the-verge',
+#     category='business',
+#     language='en',
+#     country='us'
+# )
+# # 打印新闻标题
+# for article in top_headlines['articles']:
+#     print(article['title'])
+#     print(article['url'])
+#     print('-' * 80)
+
+# news = ak.stock_news_em(symbol='600418')
+# print(news)
+
+# # 沪深300成分股
+# df_300 = ak.index_stock_cons(symbol="000300")
+# print("沪深300成分股：")
+# print(df_300.count())
+# # 中证500成分股
+# df_500 = ak.index_stock_cons(symbol="000905")
+# print("中证500成分股：")
+# print(df_500.count())
---
+++ workflow/ai_analyses_stock.py
@@ -0,0 +1,41 @@
+import akshare as ak
+from datas.fetch_stock_bars import update_daily_bars_for_code, query_daily_bars
+from tools.export import export_bars_to_csv
+from tools.log import get_analyze_logger
+from datas.query_stock import get_stock_info_by_code, format_stock_info
+from ai.ai_kbar_analyses import analyze_kbar_data_openai
+from tools.markdown_lab import save_md_to_file_name, render_markdown_to_image_file_name
+
+logger = get_analyze_logger()
+
+code = '600418'
+
+stock_info = get_stock_info_by_code(code)
+if stock_info is not None and not stock_info.empty:
+    logger.info(format_stock_info(df=stock_info))
+
+logger.info("Updating daily bars...")
+update_daily_bars_for_code(code)
+logger.info("✅ Done updating.")
+
+df = query_daily_bars(code=code, from_date='20241113')
+
+if df is not None and not df.empty:
+    path = export_bars_to_csv(df, only_base_info=True)
+    try:
+        recent_news = ak.stock_news_em(symbol=code).to_dict(orient='records')
+    except Exception as e:
+        recent_news = None
+    
+    if path is not None:
+        logger.info(f"Exported bars to {path}, starting AI analysis...")
+        md_content = analyze_kbar_data_openai(csv_file_path=path,base_info=stock_info.to_dict('list'), recent_news=recent_news)
+        if md_content:
+            pre_name = f"{code}"
+            if not stock_info.empty:
+                stock_name = stock_info.at[code, 'name']
+                if stock_name:
+                    pre_name = f"{stock_name}({code})"
+            file_name = f"{pre_name}_分析报告"
+            save_md_to_file_name(md_content, file_name)
+            render_markdown_to_image_file_name(md_content, file_name, open_folder_after=True)

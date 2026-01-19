034aa558bb729ac05c87162fb8a22ab1acbd4776
qiankailu <ntqkhh@163.com>
2026-01-20 00:38:42 +0800

    long video analyses

--- workflow/video_process/long_video_analyses.py
+++ workflow/video_process/long_video_analyses.py
@@ -4,7 +4,7 @@ from ai.ai_srt_lab import summarize_srt
 from tools.markdown_lab import save_md_to_file, render_markdown_to_image
 from draws.kline_card import make_kline_card, save_img_file
 from tools.path import export_file_path, EXPORT_PATH
-from media_factory.whisper_mlx import whisper_to_srt
+from media_factory.whisper_mlx import whisper_to_srt_long
 from media_factory.video_handler import extract_audio_from_video
 from ai.prompts.srt_prompts import ModeType
 from media_factory.yt_dlp import yt_dlp_download
@@ -13,13 +13,13 @@ import os
 
 logger = get_analyze_logger()
 
-# video_path = EXPORT_PATH / "2025-10-06 三大高爆发力的B2买入法-弹幕版1.mp4"
-# name = video_path.stem
+video_path = EXPORT_PATH / "2025-10-06 三大高爆发力的B2买入法-弹幕版3.mp4"
+name = video_path.stem
 
-video_url = "https://www.bilibili.com/video/BV1ZBkxB6EA6?spm_id_from=333.788.recommend_more_video.-1&trackid=web_related_0.router-related-2206419-kqnfh.1768739942987.291&vd_source=442a5d658bc897c6e7d62a5abe9ddb13"
-logger.info(f"⬇️ Downloading video from {video_url}...")
-video_path = yt_dlp_download(video_url, output_dir=EXPORT_PATH)
-logger.info(f"✅ Video downloaded to {video_path}")
+# video_url = "https://www.bilibili.com/video/BV1ZBkxB6EA6?spm_id_from=333.788.recommend_more_video.-1&trackid=web_related_0.router-related-2206419-kqnfh.1768739942987.291&vd_source=442a5d658bc897c6e7d62a5abe9ddb13"
+# logger.info(f"⬇️ Downloading video from {video_url}...")
+# video_path = yt_dlp_download(video_url, output_dir=EXPORT_PATH)
+# logger.info(f"✅ Video downloaded to {video_path}")
 
 
 name = video_path.stem
@@ -52,20 +52,20 @@ else:
     logger.info("📝 Transcribing audio to SRT...")
     to_srt_prompt = name
     language = "zh"
-    srt_path = whisper_to_srt(audio_path, output_dir=export_dir, language=language, prompt=to_srt_prompt)
+    srt_path = whisper_to_srt_long(audio_path, output_dir=export_dir, language=language, prompt=to_srt_prompt)
     logger.info(f"✅ Transcription completed: {srt_path}")
 
-logger.info("🧠 Summarizing SRT content with AI...")
-mode: ModeType = "summary"
-extra_prompt = "这是一个英语语法教学视频，请基于这个前提来生成内容。"
-summary = summarize_srt(srt_file_path=srt_path, mode=mode, extra_prompt=None)
-logger.info(f"✅ Summary completed.")
+# logger.info("🧠 Summarizing SRT content with AI...")
+# mode: ModeType = "summary"
+# extra_prompt = "这是一个英语语法教学视频，请基于这个前提来生成内容。"
+# summary = summarize_srt(srt_file_path=srt_path, mode=mode, extra_prompt=None)
+# logger.info(f"✅ Summary completed.")
 
-if summary:
-    md_file_path = export_dir / f"{name}_summary.md"
-    save_md_to_file(summary, md_file_path)
-    logger.info(f"💾 Summary saved to markdown file: {md_file_path}")
+# if summary:
+#     md_file_path = export_dir / f"{name}_summary.md"
+#     save_md_to_file(summary, md_file_path)
+#     logger.info(f"💾 Summary saved to markdown file: {md_file_path}")
     
-    image_path = export_dir / f"{name}_summary.png"
-    render_markdown_to_image(summary, image_path, open_folder_after=True)
-    logger.info(f"🖼️ Rendered and opened image report for {name}_summary")
+#     image_path = export_dir / f"{name}_summary.png"
+#     render_markdown_to_image(summary, image_path, open_folder_after=True)
+#     logger.info(f"🖼️ Rendered and opened image report for {name}_summary")
